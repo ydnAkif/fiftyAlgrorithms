c66adb27eaddf80b5af4404426417f0bb5d1b5c8
Akif AYDIN <akifaydin52@gmail.com>
2024-07-31 15:06:06 +0300

    added github

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,5 +1,5 @@
 #!/opt/homebrew/bin/python3
-# buuble sort algorithm
+# bubble sort algorithm
 
 def bubble_sort(arr):
     last_element_index = len(arr) - 1
@@ -18,6 +18,4 @@ def bubble_sort(arr):
 if __name__ == '__main__':
     arr_1 = [25, 21, 22, 24, 23, 27, 26]
     sorted_arr = bubble_sort(arr_1)
-    sorted(arr_1)
     print(f"Sorted array: {sorted_arr}")
-    print(f"Sorted array: {arr_1}")
